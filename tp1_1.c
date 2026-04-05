c848c73624a12744ea199ed879bd65e629deaa3b
santiagoibazan <santiagoibazan@gmail.com>
2026-04-05 19:43:52 -0300

    segundo commit punto 4 f

--- tp1_1.c
+++ tp1_1.c
@@ -1,7 +1,16 @@
 #include <stdio.h>
 
 void main() {
-    printf("Hola mundo");
+    printf("Hola mundo \n");
 
+    int num = 79014;
+    int *puntero;
+    puntero = &num;
+
+    printf("Contenido del puntero: %d \n", *puntero);
+    printf("Direccion de memoria almacenada por el puntero: %p \n", puntero);
+    printf("Direccion de memoria de la variable: %p \n", &num);
+    printf("Direccion de memoria del puntero: %p \n", &puntero);
+    printf("Tamanio de memoria de la variable: %d \n", sizeof(num));
     return;
 }
